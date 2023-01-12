fda875e62e108f9a3fc74f6eaa02b588dbbc1afa
eld3r <eld3r@bk.ru>
2023-01-12 14:30:19 +0300

    program.cs cleanup

--- Gravity/Program.cs
+++ Gravity/Program.cs
@@ -12,31 +12,11 @@ var services = new ServiceCollection();
 
 services.RegisterStuff();
 
-var balls = new List<Ball<TwoDimensionVector>>
-{
-    new Ball<TwoDimensionVector>()
-    {
-        Position = new TwoDimensionVector() { X = 18, Y= 0 },
-        Density = 5510,
-        Mass = 5.972E2,
-        //Velocity = new TwoDimensionVector() { X = -4, Y = 4 },
-    },
-
-    new Ball<TwoDimensionVector>()
-    {
-        Position = new TwoDimensionVector() { X = 0, Y= 0 },
-        Density = 3346,
-        Mass = 7.3477,
-        //Velocity = new TwoDimensionVector() { X = 3, Y = 6 },
-    },
-    
-};
-
 var serviceProvider = services.BuildServiceProvider();
 var stroboscope = serviceProvider.GetRequiredService<IStroboscope<TwoDimensionVector>>();
 
 
-balls = JobHelper2d.GenerateRandomBalls(50);
+var balls = JobHelper2d.GenerateRandomBalls(50);
 balls.Add(new Ball<TwoDimensionVector>
 {
     Mass = 1E10,
@@ -49,11 +29,6 @@ JobHelper2d.Dump(balls);
 
 stroboscope.Init(balls);
 int i = 0;
-
-//while (true)
-//{
-//    stroboscope.Step(0.1);
-//}
 
 var step = 3.0d;
 
